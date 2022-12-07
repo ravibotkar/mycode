8384dd8814a91fe6c39023166c3d9079e2efda0e
ravibotkar <ravibotkar@gmai.com>
2022-12-07 14:23:41 +0000

    learning about telnet

---
+++ telnetproof01.py
@@ -0,0 +1,12 @@
+import telnetlib
+
+HOST = "www.python.org"
+PORT = "80"
+
+telnetObj=telnetlib.Telnet(HOST,PORT, timeout=1)
+message = ("GET /index.html HTTP/1.1\nHost:"+HOST+"\n\n").encode('ascii')
+telnetObj.write(message)
+output=telnetObj.read_all()
+print(output.decode('utf-8'))
+telnetObj.close()
+
