152074bdcd0d37202be197e3b9b1a353a9ffa20d
ravibotkar <ravibotkar@gmai.com>
2022-12-07 16:06:51 +0000

    SSH and paramiko

--- paramikosftp/sftpmover.py
+++ paramikosftp/sftpmover.py
@@ -15,12 +15,21 @@ t.connect(username="bender", password="alta3")
 ## Make an sftp connection object
 sftp = paramiko.SFTPClient.from_transport(t)
 
-## iterate across the files within directory
-for x in os.listdir("/home/student/filestocopy/"): # iterate on directory contents
-  if not os.path.isdir("/home/student/filestocopy/"+x): # filter everything that is NOT a directory
-    sftp.put("/home/student/filestocopy/"+x, "/tmp/"+x) # move file to target location
+def movethemfiles(con):
+    ## iterate across the files within directory
+    for x in os.listdir("/home/student/filestocopy/"): # iterate on directory contents
+        if not os.path.isdir("/home/student/filestocopy/"+x): # filter everything that is NOT a directory
+            con.put("/home/student/filestocopy/"+x, "/tmp/"+x) # move file to target location
 
-## close the connections
-sftp.close() # close the connection
-t.close()
+     ## close the connections
+    con.close() # close the connection
+    # ^ this line needed moved one whitespace to the left :)
+    # I moved this line over to be part of this function, otherwise it closed the connection way too early
+    t.close()
+
+def main():
+    movethemfiles(sftp)
+
+main()
+
 
