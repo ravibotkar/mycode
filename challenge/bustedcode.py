f7e3f541102a04b823f17981a4e219fcb52b2afd
ravibotkar <ravibotkar@gmai.com>
2022-12-08 10:33:08 +0000

    SSH and Netmiko

---
+++ challenge/bustedcode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+import netifaces
+
+
+def delay_print(s):
+    # there's nothing wrong with this function, it's just some cool code that
+    # will print out strings one character at a time! :)
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+delay_print("Good morning, Mr. Stark. Here are your network interfaces:\n")
+print(netifaces.interfaces())
+
+for x in netifaces.interfaces():
+    try:
+        with open("challenge.log", 'a') as bar:                 # opens writeable file for logging
+           print('Logging addresses for interface x')     # prints name of interface being logged
+           print((netifaces.ifaddresses(i)[netifaces.AF_LINK])[0]['addr'], file=bar) # Prints the MAC address to file
+           print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr'], file=bar) # Prints the IP address to file
+    except:
+        print('Could not collect adapter information') # Print an error message
---
+++ nxos_ver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""RZFeeser | Alta3 Research
+   A simple script that demonstrates using Netmiko against a Cisco NX-OS device."""
+
+# standard library
+import os
+
+# python3 -m pip install --user netmiko
+from netmiko import ConnectHandler
+
+# call our router
+def main():
+    """run time code"""
+    
+#    open_connection = ConnectHandler(device_type='cisco_nxos', ip='sandbox-nxos-1.cisco.com', username='admin', password="Admin_1234!")
+
+    open_connection = ConnectHandler(device_type='cisco_nxos', ip='sw-1', username='admin', password="alta3")
+
+    my_command = open_connection.send_command("show ver")
+
+    print(my_command)
+
+## Call main()
+if __name__ == "__main__":
+    main()
+
